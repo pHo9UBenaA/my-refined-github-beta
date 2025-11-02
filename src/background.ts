98912c0473ad049561e1cb4817070da7e4e08e25
Komiya <102408484+pHo9UBenaA@users.noreply.github.com>
2025-11-02 18:44:55 +0900

    v0.4.0 (#4)
    * update focusSearchBox
    
    * chore
    
    * Add copy-SSH command to extension bundle

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,12 @@
 /// <reference types="@types/chrome" />
 
 import { focusSearchBox } from "./features/focusSearchBox.ts";
+import { copySshUri } from "./features/copySshUri.ts";
 
 chrome.commands.onCommand.addListener(async (command: string) => {
   if (command === "focus_search") {
     await focusSearchBox();
+  } else if (command === "copy_ssh_uri") {
+    await copySshUri();
   }
 });
